008fbfd57b9a3ae83dd2d2459edcf17b7459a0ee
Paul Jackob <pauljackom911@hotmail.com>
2018-07-05 22:57:55 +0200

    modified js file

--- scrape.js
+++ scrape.js
@@ -3,3 +3,24 @@
 // Look at https://github.com/HackYourFutureBelgium/JavaScript2/tree/master/Projects/web-scraper
 // for the version we showed in class.
 
+
+document.addEventListener("keypress", function() {
+let table = document.querySelector("table.cast_list");
+let rows = table.querySelectorAll("tr.odd, tr.even");
+
+ for (let i = 0; i < rows.length; i++) {
+    const row = rows[i];
+    let actor = row.querySelector("[itemprop=actor] a span").innerHTML;
+    let characterTag = row.querySelector("td.character");
+    let character;
+    let a = characterTag.querySelector("a");
+
+    if (a != null) {
+      character = a.innerHTML;
+    } else {
+      character = characterTag.innerHTML;
+    }
+
+    console.log("Actor: " + actor.trim() + ", Character: " + character.trim());
+  }
+});
